b7fd9899226e9a82ff92a1afeb4a282e7f7c1f42
CHI\cskjs <kent@seaplanehk.com>
2023-05-20 10:02:48 +0800

    install axios fetch pokemon handler add offset and limit add load more functionality styling

--- SpectrumPokedex/src/screens/HomeScreen/HomeScreen.js
+++ SpectrumPokedex/src/screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, BackHandler } from "react-native";
+import {
+  View,
+  FlatList,
+  Image,
+  Text,
+  Button,
+  ActivityIndicator,
+  BackHandler,
+  StyleSheet,
+} from "react-native";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0);
+
   useEffect(() => {
+    fetchPokemon();
     const backAction = () => {
       // Prevent going back to the login screen
       return true;
@@ -16,17 +31,89 @@ const HomeScreen = () => {
     return () => backHandler.remove();
   }, []);
 
+  const fetchPokemon = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=10`
+      );
+      const data = response.data.results;
+      setPokemonList((prevList) => [...prevList, ...data]);
+      setLoading(false);
+    } catch (error) {
+      console.log("Error fetching Pokemon list:", error);
+      setLoading(false);
+    }
+  };
+
+  const handleLoadMore = () => {
+    setOffset((prevOffset) => prevOffset + 10);
+  };
+
+  const renderFooter = () => {
+    if (!loading) return null;
+    return <ActivityIndicator size="large" color="blue" />;
+  };
+
+  const renderItem = ({ item, index }) => {
+    const pokemonNumber = index + 1;
+    return (
+      <View style={styles.itemContainer}>
+        <View style={styles.numberContainer}>
+          <Text style={styles.numberText}>{pokemonNumber}</Text>
+        </View>
+        <Image
+          source={{
+            uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonNumber}.png`,
+          }}
+          style={styles.itemImage}
+        />
+        <Text style={styles.itemName}>{item.name}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.root}>
-      <Text>Home</Text>
+      <FlatList
+        data={pokemonList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ListFooterComponent={renderFooter}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.5}
+      />
+      <Button title="Load More" onPress={handleLoadMore} disabled={loading} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#f5f5f5",
+  },
+  listContainer: {
+    paddingBottom: 10,
+  },
+  itemContainer: {
+    flexDirection: "row",
     alignItems: "center",
-    padding: 100,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 10,
+  },
+  itemImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  itemName: {
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
